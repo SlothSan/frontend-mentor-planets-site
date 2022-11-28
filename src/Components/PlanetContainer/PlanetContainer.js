a90990cab0e62a5bf3c963fb7cbe51a82451958d
Mike Oram <Mikeoram01@gmail.com>
2022-11-28 18:01:22 +0000

    Added the info cards to every view

--- src/Components/PlanetContainer/PlanetContainer.js
+++ src/Components/PlanetContainer/PlanetContainer.js
@@ -7,6 +7,24 @@ const PlanetContainer = (props) => {
             <Link to={"/" + props.planetData.name.toLowerCase() + "/overview"}>Overview</Link>
             <Link to={"/" + props.planetData.name.toLowerCase() + "/structure"}>Structure</Link>
             <Link to={"/" + props.planetData.name.toLowerCase() + "/geology"}>Geology</Link>
+            <div className={"info-container"}>
+                <div className={"info-card"}>
+                    <p className={"info-title"}>ROTATION TIME</p>
+                    <p className={"info-text"}>{props.planetData.rotation}</p>
+                </div>
+                <div className={"info-card"}>
+                    <p className={"info-title"}>REVOLUTION TIME</p>
+                    <p className={"info-text"}>{props.planetData.revolution}</p>
+                </div>
+                <div className={"info-card"}>
+                    <p className={"info-title"}>RADIUS</p>
+                    <p className={"info-text"}>{props.planetData.radius}</p>
+                </div>
+                <div className={"info-card"}>
+                    <p className={"info-title"}>AVERAGE TEMP.</p>
+                    <p className={"info-text"}>{props.planetData.temperature}</p>
+                </div>
+            </div>
         </section>
     )
 }
