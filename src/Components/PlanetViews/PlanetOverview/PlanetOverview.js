324ae4a3b85b7db7e85b66d06adf976b753d8a0a
Mike Oram <Mikeoram01@gmail.com>
2022-11-29 13:12:34 +0000

    Tablet styling in progress for overview view can then copy it over to the other views

--- src/Components/PlanetViews/PlanetOverview/PlanetOverview.js
+++ src/Components/PlanetViews/PlanetOverview/PlanetOverview.js
@@ -8,14 +8,16 @@ const PlanetOverview = (props) => {
                 <img className={"planet-image"} src={props.planetData.images.planet}
                      alt={`Image of ${props.planetData.name} overview`}/>
             </div>
-            <div className={"planet-text-container"}>
-                <p className={"planet-name"}>{props.planetData.name.toUpperCase()}</p>
-                <p className={"planet-text"}>{props.planetData.overview.content}</p>
-                <p className={"source-text"}>Source : <a className={"source-link"}
-                                                         href={props.planetData.overview.source}
-                                                         target={"_blank"}>Wikipedia<img
-                    src={"../imgs/icon-source.svg"}
-                    alt={"source icon"}/></a></p>
+            <div className={"planet-text-container"}>\
+                <div className={"planet-info-container"}>
+                    <p className={"planet-name"}>{props.planetData.name.toUpperCase()}</p>
+                    <p className={"planet-text"}>{props.planetData.overview.content}</p>
+                    <p className={"source-text"}>Source : <a className={"source-link"}
+                                                             href={props.planetData.overview.source}
+                                                             target={"_blank"}>Wikipedia<img
+                        src={"../imgs/icon-source.svg"}
+                        alt={"source icon"}/></a></p>
+                </div>
                 <Links planetData={props.planetData}/>
             </div>
         </div>
