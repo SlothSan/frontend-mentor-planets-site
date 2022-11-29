a1a45acf8957b06e53cfb7eef25c0dfa5f4672d3
Mike Oram <Mikeoram01@gmail.com>
2022-11-29 12:18:09 +0000

    Styling cahnges

--- src/Components/PlanetContainer/PlanetContainer.js
+++ src/Components/PlanetContainer/PlanetContainer.js
@@ -6,21 +6,23 @@ const PlanetContainer = (props) => {
         <section className={"planet-container"}>
             <Outlet/>
             <div className={"info-container"}>
-                <div className={"info-card"}>
-                    <p className={"info-title"}>ROTATION TIME</p>
-                    <p className={"info-text"}>{props.planetData.rotation}</p>
-                </div>
-                <div className={"info-card"}>
-                    <p className={"info-title"}>REVOLUTION TIME</p>
-                    <p className={"info-text"}>{props.planetData.revolution}</p>
-                </div>
-                <div className={"info-card"}>
-                    <p className={"info-title"}>RADIUS</p>
-                    <p className={"info-text"}>{props.planetData.radius}</p>
-                </div>
-                <div className={"info-card"}>
-                    <p className={"info-title"}>AVERAGE TEMP.</p>
-                    <p className={"info-text"}>{props.planetData.temperature}</p>
+                <div className={"info-card-container"}>
+                    <div className={"info-card"}>
+                        <p className={"info-title"}>ROTATION TIME</p>
+                        <p className={"info-text"}>{props.planetData.rotation}</p>
+                    </div>
+                    <div className={"info-card"}>
+                        <p className={"info-title"}>REVOLUTION TIME</p>
+                        <p className={"info-text"}>{props.planetData.revolution}</p>
+                    </div>
+                    <div className={"info-card"}>
+                        <p className={"info-title"}>RADIUS</p>
+                        <p className={"info-text"}>{props.planetData.radius}</p>
+                    </div>
+                    <div className={"info-card"}>
+                        <p className={"info-title"}>AVERAGE TEMP.</p>
+                        <p className={"info-text"}>{props.planetData.temperature}</p>
+                    </div>
                 </div>
             </div>
         </section>
