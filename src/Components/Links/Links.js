d58ac825c4bca83f982f9168d1ce3b2414566f34
Mike Oram <Mikeoram01@gmail.com>
2022-11-29 11:18:54 +0000

    Added logic to check path and change state of links depending on path

--- src/Components/Links/Links.js
+++ src/Components/Links/Links.js
@@ -1,14 +1,27 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {useState, useEffect} from "react";
 
 const Links = (props) => {
+
+    const [currentPath, setCurrentPath] = useState('');
+
+    const location = useLocation();
+
+    useEffect(() => {
+        setCurrentPath(location.pathname);
+    }, [location])
+
     return (
         <div className={"internal-link-container"}>
-            <Link className={"internal-link"}
-                  to={"/" + props.planetData.name.toLowerCase() + "/overview"}><span>01</span>Overview</Link>
-            <Link className={"internal-link"}
-                  to={"/" + props.planetData.name.toLowerCase() + "/structure"}><span>02</span>Structure</Link>
-            <Link className={"internal-link"}
-                  to={"/" + props.planetData.name.toLowerCase() + "/geology"}><span>03</span>Geology</Link>
+            <Link
+                className={currentPath.includes("overview") ? `${props.planetData.name.toLowerCase()}-active internal-link` : `internal-link`}
+                to={"/" + props.planetData.name.toLowerCase() + "/overview"}><span>01</span>Overview</Link>
+            <Link
+                className={currentPath.includes("structure") ? `${props.planetData.name.toLowerCase()}-active internal-link` : `internal-link`}
+                to={"/" + props.planetData.name.toLowerCase() + "/structure"}><span>02</span>Structure</Link>
+            <Link
+                className={currentPath.includes("geology") ? `${props.planetData.name.toLowerCase()}-active internal-link` : `internal-link`}
+                to={"/" + props.planetData.name.toLowerCase() + "/geology"}><span>03</span>Geology</Link>
         </div>
     )
 }
