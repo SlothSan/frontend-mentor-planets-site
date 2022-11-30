8dd623072e0a946277eee10395e9e3544291765f
Mike Oram <Mikeoram01@gmail.com>
2022-11-30 11:30:26 +0000

    Created a component for mobile internal links

--- src/Components/PlanetViews/PlanetOverview/PlanetOverview.js
+++ src/Components/PlanetViews/PlanetOverview/PlanetOverview.js
@@ -1,8 +1,10 @@
 import Links from "../../Links/Links";
+import MobileLinks from "../../MobileLinks/MobileLinks";
 
 const PlanetOverview = (props) => {
     return (
         <div className={"planet-sub-container"}>
+            <MobileLinks className={"mobile-link-container"} planetData={props.planetData}/>
             <div className={"planet-image-container"}>
                 <img className={"planet-image"} src={props.planetData.images.planet}
                      alt={`Image of ${props.planetData.name} overview`}/>
@@ -17,7 +19,7 @@ const PlanetOverview = (props) => {
                         src={"../imgs/icon-source.svg"}
                         alt={"source icon"}/></a></p>
                 </div>
-                <Links planetData={props.planetData}/>
+                <Links className={"internal-link-container"} planetData={props.planetData}/>
             </div>
         </div>
     )
